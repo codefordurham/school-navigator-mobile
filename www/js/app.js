47eab39bbd6b6befa3937d7f4334fda2b414e4ce
David Ray <davidray@gmail.com>
2015-10-09 14:13:42 -0400

    Initial ShipIt Day commit

---
+++ www/js/app.js
@@ -0,0 +1,64 @@
+(function() {
+var app = angular.module('schools', ['ionic']);
+
+// place to setup data to display in view
+app.controller('SchoolsCtrl', function($http, $scope) {
+
+  $scope.charter = [];
+  $scope.magnet = [];
+  $scope.assigned = [];
+  $scope.levels = ['elementary', 'secondary', 'middle', 'high'];
+
+  $scope.form = { query: '' };
+
+  $scope.search = function() {
+
+    if ($scope.form.query) {
+      var lookup_address = $scope.form.query;
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode( { 'address': lookup_address}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          var geo = results[0].geometry.location;
+          $http.get('https://schools.codefordurham.com/api/schools/?format=json&longitude=' + geo.lng() + '&latitude=' + geo.lat())
+            .success(function(response) {
+              $scope.charter = [];
+              $scope.magnet = [];
+              $scope.assigned = [];
+              angular.forEach(response, function(school) {
+                // TODO: Add back in 'specialty' when the desktop front end bug is fixed
+                if (school.eligibility === 'option') {
+                  if (school.type === 'charter') {
+                    $scope.charter.push(school);
+                  } else if (school.type === 'magnet') {
+                    $scope.magnet.push(school);
+                  }
+                } else {
+                  $scope.assigned.push(school);
+                }
+              });
+            });
+
+        } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+    }
+  };
+
+
+
+});
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+}());
